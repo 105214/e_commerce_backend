45e89ca799540acfc540d1daaf46eca06c243181
Nithin sukumar <nithinsukumar200@gmail.com>
2025-06-16 08:02:13 +0530

    create user done

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mangodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app =express()
 const port = process.env.PORT || 3000
 connectDB()
 connectCloudinary()
 
+//middleware
 app.use(express.json())
 app.use(cors())
 
+
+//api endpoint
+app.use('/api/user',userRouter)
 app.listen(port,()=>{
     console.log(`server running on ${port}`);
     
